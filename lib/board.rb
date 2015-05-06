3a6e271ec72c148505c7941c86d6f5f6f0a32ad0
MollieS <mollie.c.stephenson@gmail.com>
2015-05-06 13:49:22 +0100

    added grid_length method

--- lib/board.rb
+++ lib/board.rb
@@ -3,12 +3,19 @@ class Board
   attr_reader :grid
 
   def initialize
-    @grid = []
+    @grid = [{ :A => 0 }]
   end
 
-  def place_ship ship, orientation
+  def place_ship ship, coords, orientation
+    grid_length ship, coords, orientation
     ship.orientation = orientation
     @grid << ship
+  end
+
+  def grid_length ship, coords, orientation
+    if orientation == 'v'
+      fail 'boat is off the grid!' if (coords - 1) + ship.length > grid.length
+    end
   end
 
 end
--- spec/features/features_test_spec.rb
+++ spec/features/features_test_spec.rb
@@ -8,14 +8,14 @@ feature 'Play a game of battleships' do
   let(:ship) { Ship.new }
 
   scenario 'put a ship on the board with a given orientation' do
-    board.place_ship ship, 'v'
+    board.place_ship ship, 'A1', 'v'
     expect(board.grid).to include ship
     expect(ship.orientation).to eq 'v'
   end
 
   scenario 'ensure ship does not go off edge of grid' do
-    destroyer = ship 2
-    expect { board.place_ship destroyer, 'v', 'A1' }.to raise_error 'boat is off the grid!'
+    destroyer = Ship.new 2
+    expect { board.place_ship destroyer, 'A1', 'v'}.to raise_error 'boat is off the grid!'
   end
 
 end
