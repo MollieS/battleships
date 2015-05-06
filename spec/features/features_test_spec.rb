004000bf40ecdf1a78096ae2d01e3cd60797ea51
andygout <andygout@hotmail.co.uk>
2015-05-06 11:04:58 +0100

    Added: feature test to include ship

---
+++ spec/features/features_test_spec.rb
@@ -0,0 +1,15 @@
+require 'capybara/rspec'
+require 'board'
+require 'ship'
+
+feature 'Play a game of battleships' do
+
+  let(:board) { Board.new }
+  let(:ship) { Ship.new }
+
+  scenario 'put a ship on the board' do
+    board.place_ship ship
+    expect(board.grid).to include ship
+  end
+
+end
